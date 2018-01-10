60ffbb82b1f368afd455cff2cdeed00cdae19b35
vincent <xu7665@gmail.com>
2018-01-10 22:52:05 +0800

    vueshop

---
+++ VueShop/apps/user_operation/models.py
@@ -0,0 +1,65 @@
+from datetime import datetime
+from django.db import models
+from django.contrib.auth import get_user_model
+from goods.models import Goods
+# Create your models here.
+User = get_user_model()
+
+class UserFav(models.Model):
+    """
+    用户收藏
+    """
+    user = models.ForeignKey(User,verbose_name="用户")
+    goods = models.ForeignKey(Goods,verbose_name="商品")
+    add_time = models.DateTimeField(default=datetime.now,verbose_name="商品")
+
+    class Meta:
+        verbose_name = '用户收藏'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.user.name
+
+
+class UserLeavingMesage(models.Model):
+    """
+    用户留言
+    """
+    MESSAGE_CHOICES = (
+        (1,"留言"),
+        (2,"投诉"),
+        (3,"询问"),
+        (4,"售后"),
+        (5,"求购")
+    )
+    user = models.ForeignKey(User,verbose_name="用户")
+    message_type = models.IntegerField(default=1,choices=MESSAGE_CHOICES,verbose_name="留言类型",
+                                       help_text=u"留言类型:1(留言),2(投诉),3(询问),4(售后),5(求购)")
+    subjuct = models.CharField(max_length=100,default="",verbose_name="主题")
+    file = models.TextField(default="",verbose_name="留言内容",help_text="留言内容")
+    add_time = models.DateTimeField(default=datetime.now,verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "用户留言 "
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.subjuct
+
+class UserAddress(models.Model):
+    """
+    用户收货地址
+    """
+    user = models.ForeignKey(User,verbose_name="用户")
+    address = models.CharField(max_length=100,default="",verbose_name="地址")
+    district = models.CharField(max_length=100,default="",verbose_name="区域")
+    signer_name = models.CharField(max_length=100,default="",verbose_name="签收人")
+    signer_mobile = models.CharField(max_length=11,default="",verbose_name="电话")
+    add_time = models.DateTimeField(default=datetime.now,verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "收货地址"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.address
